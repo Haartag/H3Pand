5234980969a577e88ac2e690be89b394643db891
llin first <90470991+Haartag@users.noreply.github.com>
2022-08-26 20:31:34 +0300

    GetBoxWithPercentUseCase and tests added

---
+++ app/src/main/java/com/valerytimofeev/h3pand/domain/use_case/GetBoxWithPercentUseCase.kt
@@ -0,0 +1,121 @@
+package com.valerytimofeev.h3pand.domain.use_case
+
+import com.valerytimofeev.h3pand.data.local.BoxValueItem
+import com.valerytimofeev.h3pand.utils.*
+import javax.inject.Inject
+import kotlin.math.roundToInt
+
+/**
+ * Get drop chances for valid box.
+ * Box obtained from GetBoxForGuardRangeUseCase
+ */
+class GetBoxWithPercentUseCase @Inject constructor(
+
+) {
+    operator fun invoke(
+        boxValueItem: BoxValueItem,
+        guardValue: GuardCharacteristics,
+        difficult: Difficult,
+        unitValue: Int,
+        week: Int,
+        chosenGuardRange: IntRange,
+        additionalValue: Int
+    ): Resource<BoxWithDropPercent> {
+
+        val sumValue = boxValueItem.value + additionalValue
+
+        val midGuardValue =
+            if ((sumValue - difficult.minValue2) * difficult.coefficient2 > 0) {
+                ((sumValue - difficult.minValue1) * difficult.coefficient1 + (sumValue - difficult.minValue2) * difficult.coefficient2).roundToInt()
+            } else {
+                ((sumValue - difficult.minValue1) * difficult.coefficient1).roundToInt()
+            }
+        val lowGuardNumber =
+            ((midGuardValue * 0.75).ceilToInt() / unitValue.toDouble()).ceilToInt()
+        val hiGuardNumber = ((midGuardValue * 1.25).toInt() / unitValue.toDouble()).toInt()
+
+        val numbersList = (lowGuardNumber..hiGuardNumber).map { it }
+
+        val numbersWithPercentsMap = numbersList.getPercents()
+
+        val guards = mutableListOf<Int>()
+        var summaryPercent = 0.0
+
+        /**
+         * Adding all valid boxes to list and summing up their percentages
+         */
+        run breaking@{
+            numbersWithPercentsMap.forEach {
+                if (
+                    it.key in guardValue.minRoll..guardValue.maxRoll && it.key.weekCorrectionUndo(
+                        week
+                    ) in chosenGuardRange
+                ) {
+                    summaryPercent += it.value
+                    guards.add(it.key.weekCorrectionUndo(week))
+                    /**
+                     * For some week values, the calculated upper range value will be less than
+                     * the upper guardRange value (for example, 48 instead of 49, etc.).
+                     * This is because some values are not possible when calculating
+                     * the week-adjusted value. But the user expects the upper range
+                     * (when going beyond the guardRange) will be equal to the upper range
+                     * of the guardRange, not another number.
+                     *
+                     * Therefore, a fake value equal to the upper limit of guardRange is added to
+                     * guards, with zero drop percent.
+                     * Tests that take this trick into account are marked with !!.
+                     */
+                } else if (it.key.weekCorrectionUndo(week) > chosenGuardRange.last && guards.isNotEmpty()) {
+                    guards.add(chosenGuardRange.last)
+                    return@breaking
+                }
+            }
+        }
+
+        if (guards.isEmpty()) return Resource.error("An unknown error occurred", null)
+
+        return Resource.success(
+            BoxWithDropPercent(
+                boxValueItem.boxContent,
+                summaryPercent,
+                guards.first()..guards.last(),
+                boxValueItem.img
+            )
+        )
+    }
+
+    private fun List<Int>.getPercents(): Map<Int, Double> {
+
+        var divider = 1
+
+        /**
+         * Calculate drop chance. Each 2 additional values gives +1 to previous item chance
+         * and + item count to divider.
+         * ex. 1/1 -> 1/4 - 2/4 - 1/4 -> 1/9 - 2/9 - 3/9 - 2/9 - 1/9 e.t.c.
+         */
+        for (index in 1 until this.size step 2) {
+            divider += (2 + index)
+        }
+
+        val percentList = mutableListOf<Double>()
+
+        for (i in 1..this.size / 2 + 1) {
+            percentList.add((i.toDouble() / divider) * 100)
+        }
+        for (i in this.size / 2 downTo 1) {
+            percentList.add((i.toDouble() / divider) * 100)
+        }
+
+        return this.zip(percentList).toMap()
+    }
+
+    private fun Int.weekCorrectionUndo(
+        week: Int
+    ): Int {
+        var weekModifier = 1.0
+        repeat(week - 1) {
+            weekModifier *= 1.1
+        }
+        return (this * weekModifier).ceilToInt()
+    }
+}
---
+++ app/src/test/java/com/valerytimofeev/h3pand/domain/use_case/GetBoxWithPercentUseCaseTest.kt
@@ -0,0 +1,118 @@
+package com.valerytimofeev.h3pand.domain.use_case
+
+import com.google.common.truth.Truth.assertThat
+import com.valerytimofeev.h3pand.data.local.BoxValueItem
+import com.valerytimofeev.h3pand.utils.*
+import org.junit.Before
+import org.junit.Test
+
+class GetBoxWithPercentUseCaseTest() {
+
+    private lateinit var getBoxWithPercentUseCase: GetBoxWithPercentUseCase
+
+    @Before
+    fun setup() {
+        getBoxWithPercentUseCase = GetBoxWithPercentUseCase()
+    }
+
+    @Test
+    fun `Get BoxWithDropPercent, valid input, all guards in range`() {
+        val boxValueItem = BoxValueItem(1, "5000exp", 6000, "exp")
+        val guardValue = GuardCharacteristics(20, 4020, 65, 13065)
+        val difficult = Difficult.THREE
+        val unitValue = 190
+        val week = 1
+        val additionalValue = 0
+        val chosenGuardRange = 20..49
+
+        val value = getBoxWithPercentUseCase(
+            boxValueItem, guardValue, difficult, unitValue, week, chosenGuardRange, additionalValue
+        )
+        assertThat(value.data).isEqualTo(BoxWithDropPercent(name="5000exp", dropChance=100.0, range = 20..32, img="exp"))
+    }
+
+    @Test
+    fun `Get BoxWithDropPercent, valid input, some guards not in range`() {
+        val boxValueItem = BoxValueItem(1, "5000exp", 6000, "exp")
+        val guardValue = GuardCharacteristics(25, 4020, 65, 13065)
+        val difficult = Difficult.THREE
+        val unitValue = 201
+        val week = 1
+        val additionalValue = 0
+        val chosenGuardRange = 20..49
+
+        val value = getBoxWithPercentUseCase(
+            boxValueItem, guardValue, difficult, unitValue, week, chosenGuardRange, additionalValue
+        )
+
+        assertThat(value.data!!.dropChance).isAtLeast(57.0)
+        assertThat(value.data!!.dropChance).isAtMost(57.5)
+    }
+
+    @Test
+    fun `Get BoxWithDropPercent, too high value, returns error`() {
+        val boxValueItem = BoxValueItem(1, "1LvlSpells", 5000, "1LvlSpells")
+        val guardValue = GuardCharacteristics(20, 20, 65, 65)
+        val difficult = Difficult.THREE
+        val unitValue = 1350
+        val week = 1
+        val additionalValue = 0
+        val chosenGuardRange = 20..49
+
+        val value = getBoxWithPercentUseCase(
+            boxValueItem, guardValue, difficult, unitValue, week, chosenGuardRange, additionalValue
+        )
+
+        assertThat(value).isEqualTo(Resource.error("An unknown error occurred", null))
+    }
+
+    @Test
+    fun `Get BoxWithDropPercent, valid input, week after 1`() {
+        val boxValueItem = BoxValueItem(1, "5000exp", 6000, "exp")
+        val guardValue = GuardCharacteristics(20, 4020, 65, 13065)
+        val difficult = Difficult.THREE
+        val unitValue = 190
+        val week = 3
+        val additionalValue = 0
+        val chosenGuardRange = 20..49
+
+        val value = getBoxWithPercentUseCase(
+            boxValueItem, guardValue, difficult, unitValue, week, chosenGuardRange, additionalValue
+        )
+        assertThat(value.data).isEqualTo(BoxWithDropPercent(name="5000exp", dropChance=100.0, range = 25..39, img="exp"))
+    }
+
+    @Test
+    fun `!! Get BoxWithDropPercent, valid input, part of week-modified range above guard range`() {
+        val boxValueItem = BoxValueItem(1, "5000exp", 6000, "exp")
+        val guardValue = GuardCharacteristics(20, 4020, 65, 13065)
+        val difficult = Difficult.THREE
+        val unitValue = 190
+        val week = 7
+        val additionalValue = 0
+        val chosenGuardRange = 20..49
+
+        val value = getBoxWithPercentUseCase(
+            boxValueItem, guardValue, difficult, unitValue, week, chosenGuardRange, additionalValue
+        )
+        assertThat(value.data!!.range).isEqualTo((36..49))
+        assertThat(value.data!!.dropChance).isAtLeast(69.0)
+        assertThat(value.data!!.dropChance).isAtMost(70.0)
+    }
+
+    @Test
+    fun `!! Get BoxWithDropPercent, valid input, all week-modified range above guard range`() {
+        val boxValueItem = BoxValueItem(1, "5000exp", 6000, "exp")
+        val guardValue = GuardCharacteristics(20, 4020, 65, 13065)
+        val difficult = Difficult.THREE
+        val unitValue = 190
+        val week = 11
+        val additionalValue = 0
+        val chosenGuardRange = 20..49
+
+        val value = getBoxWithPercentUseCase(
+            boxValueItem, guardValue, difficult, unitValue, week, chosenGuardRange, additionalValue
+        )
+        assertThat(value).isEqualTo(Resource.error("An unknown error occurred", null))
+    }
+}
