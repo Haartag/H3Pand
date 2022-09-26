673155e1ece7f75dcf651d577e49346a1586e9a8
llin first <90470991+Haartag@users.noreply.github.com>
2022-09-26 16:28:30 +0300

    MapSettings updated, added zone settings for better support zone modifiers.

--- app/src/main/java/com/valerytimofeev/h3pand/utils/MapSettings.kt
+++ app/src/main/java/com/valerytimofeev/h3pand/utils/MapSettings.kt
@@ -1,15 +1,31 @@
 package com.valerytimofeev.h3pand.utils
 
+data class ZoneSettings(
+    val zoneName: String,
+    val difficult: Int,
+    val valueRange: IntRange
+)
+
 enum class MapSettings(
     val mapName: String,
     val numberOfZones: Int,
-    val valueRanges: List<IntRange>,
+    val valueRanges: List<ZoneSettings>,
+    val expRate: Int = 20,
+    val goldRate: Int = 5,
+    val spellRate: Int = 2,
+    val unitRate: Int = 3,
+
     val mirror: Boolean = false
 ) {
     /**
      * Value range must be over 2000
      */
-    JC("Jebus Cross", 5, listOf(2000..22000, 10000..55000));
+    JC(
+        "Jebus Cross", 5, listOf(
+            ZoneSettings("respawn", 3, 2000..22000),
+            ZoneSettings("mid", 5, 10000..55000),
+        )
+    );
 
     companion object {
         fun getMapSettings(mapName: String) = values().find { it.name == mapName.uppercase() }
