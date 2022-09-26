6e17622d94d3b08cc462716cfeaa4805a9ada654
llin first <90470991+Haartag@users.noreply.github.com>
2022-09-26 16:38:24 +0300

    MainActivity experimental material API support added

--- app/src/main/java/com/valerytimofeev/h3pand/MainActivity.kt
+++ app/src/main/java/com/valerytimofeev/h3pand/MainActivity.kt
@@ -4,16 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.valerytimofeev.h3pand.pandcalculation.PandCalculationScreen
+import com.valerytimofeev.h3pand.ui.pandcalculation.PandCalculationScreen
 import com.valerytimofeev.h3pand.ui.theme.H3PandTheme
 import dagger.hilt.android.AndroidEntryPoint
 
+@ExperimentalMaterialApi
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
