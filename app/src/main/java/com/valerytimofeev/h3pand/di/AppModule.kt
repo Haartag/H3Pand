70a666cd344e402f0f3f05f9edd7da6ca7959530
llin first <90470991+Haartag@users.noreply.github.com>
2022-09-26 16:01:42 +0300

    DI AppModule updated. UseCase provide added.

--- app/src/main/java/com/valerytimofeev/h3pand/di/AppModule.kt
+++ app/src/main/java/com/valerytimofeev/h3pand/di/AppModule.kt
@@ -4,8 +4,13 @@ import android.content.Context
 import androidx.room.Room
 import com.valerytimofeev.h3pand.data.local.PandDao
 import com.valerytimofeev.h3pand.data.local.PandDatabase
+import com.valerytimofeev.h3pand.domain.use_case.GetBoxForGuardRangeUseCase
+import com.valerytimofeev.h3pand.domain.use_case.GetBoxWithPercentUseCase
+import com.valerytimofeev.h3pand.domain.use_case.GetGuardCharacteristicsUseCase
 import com.valerytimofeev.h3pand.repositories.local.DefaultPandRepository
 import com.valerytimofeev.h3pand.repositories.local.PandRepository
+import com.valerytimofeev.h3pand.utils.GuardCharacteristics
+import com.valerytimofeev.h3pand.utils.Resource
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -41,4 +46,21 @@ object AppModule {
     fun provideDefaultStorageRepository(
         dao: PandDao
     ) = DefaultPandRepository(dao) as PandRepository
+
+
+    /**
+     * Use case DI
+     */
+
+    @Singleton
+    @Provides
+    fun provideGetGuardCharacteristicsUseCase(): GetGuardCharacteristicsUseCase {
+        return GetGuardCharacteristicsUseCase()
+    }
+
+    @Singleton
+    @Provides
+    fun provideGetBoxWithPercentUseCase(): GetBoxWithPercentUseCase {
+        return GetBoxWithPercentUseCase()
+    }
 }
