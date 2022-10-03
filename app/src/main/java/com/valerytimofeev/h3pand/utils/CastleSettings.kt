14b43c1d016dce0b9b9d9c30ef7dfcd90f61e979
llin first <90470991+Haartag@users.noreply.github.com>
2022-10-03 21:41:27 +0300

    Castle vector images and CastleSettings added

--- app/src/main/java/com/valerytimofeev/h3pand/utils/CastleSettings.kt
+++ app/src/main/java/com/valerytimofeev/h3pand/utils/CastleSettings.kt
@@ -2,7 +2,7 @@ package com.valerytimofeev.h3pand.utils
 
 import androidx.compose.ui.graphics.Color
 import com.valerytimofeev.h3pand.R
-import com.valerytimofeev.h3pand.ui.theme.Secondary
+import com.valerytimofeev.h3pand.ui.theme.*
 
 enum class CastleSettings(
     val id: Int,
@@ -10,15 +10,15 @@ enum class CastleSettings(
     val img: Int,
     val sheetColor: Color
 ) {
-    CAS(1, "Castle", R.drawable.ic_castle, Secondary),
-    CON(2, "Conflux", R.drawable.ic_placeholder, Secondary),
-    COV(3, "Cove", R.drawable.ic_placeholder, Secondary),
-    DUN(4, "Dungeon", R.drawable.ic_placeholder, Secondary),
-    FOR(5, "Fortress", R.drawable.ic_placeholder, Secondary),
-    INF(6, "Inferno", R.drawable.ic_placeholder, Color.LightGray),
-    NEC(7, "Necropolis", R.drawable.ic_placeholder, Secondary),
-    RAM(8, "Rampart", R.drawable.ic_rampart, Color.Green),
-    STR(9, "Stronghold", R.drawable.ic_placeholder, Secondary),
-    TOW(10, "Tower", R.drawable.ic_tower, Secondary),
-    NEU(0, "Neutral", R.drawable.ic_placeholder, Secondary),
+    CAS(1, "Castle", R.drawable.town_castle, CastleColor),
+    CON(2, "Conflux", R.drawable.town_conflux, ConfluxColor),
+    COV(3, "Cove", R.drawable.town_cove, CoveColor),
+    DUN(4, "Dungeon", R.drawable.town_dungeon, DungeonColor),
+    FOR(5, "Fortress", R.drawable.town_fortress, FortressColor),
+    INF(6, "Inferno", R.drawable.town_inferno, InfernoColor),
+    NEC(7, "Necropolis", R.drawable.town_necropolis, NecropolisColor),
+    RAM(8, "Rampart", R.drawable.town_rampart, RampartColor),
+    STR(9, "Stronghold", R.drawable.town_stronghold, StrongholdColor),
+    TOW(10, "Tower", R.drawable.town_tower, TowerColor),
+    NEU(0, "Neutral", R.drawable.town_neutral, NeutralColor),
 }
