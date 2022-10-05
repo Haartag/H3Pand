f1804c959651c9c853ae19b3302efab6b993cd88
llin first <90470991+Haartag@users.noreply.github.com>
2022-10-05 18:08:44 +0300

    Colors

--- app/src/main/java/com/valerytimofeev/h3pand/ui/theme/Theme.kt
+++ app/src/main/java/com/valerytimofeev/h3pand/ui/theme/Theme.kt
@@ -6,16 +6,16 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
+/*private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
     secondary = Teal200
-)
+)*/
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Secondary
 
     /* Other default colors to override
     background = Color.White,
@@ -30,7 +30,8 @@ private val LightColorPalette = lightColors(
 @Composable
 fun H3PandTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
-        DarkColorPalette
+        //DarkColorPalette
+        LightColorPalette
     } else {
         LightColorPalette
     }
