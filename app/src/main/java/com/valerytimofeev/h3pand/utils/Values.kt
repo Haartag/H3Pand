ffdb78215ad5ff1122bafe314bd05069c0851552
llin first <90470991+Haartag@users.noreply.github.com>
2022-10-05 21:23:50 +0300

    Custom values

---
+++ app/src/main/java/com/valerytimofeev/h3pand/utils/Values.kt
@@ -0,0 +1,16 @@
+package com.valerytimofeev.h3pand.utils
+
+object Values {
+    val valueList = listOf(
+        500,
+        1000,
+        1500,
+        2000,
+        2500,
+        3000,
+        5000,
+        10000,
+        15000,
+        20000,
+    )
+}
