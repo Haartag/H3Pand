d4c7a1d6d1378814f7f43fe093073b015f195c63
llin first <90470991+Haartag@users.noreply.github.com>
2022-10-06 17:25:24 +0300

    New fonts

--- app/src/main/java/com/valerytimofeev/h3pand/ui/theme/Type.kt
+++ app/src/main/java/com/valerytimofeev/h3pand/ui/theme/Type.kt
@@ -2,27 +2,66 @@ package com.valerytimofeev.h3pand.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.valerytimofeev.h3pand.R
 
-// Set of Material typography styles to start with
+val Overpass = FontFamily(
+    Font(R.font.overpass_light, FontWeight.Light),
+    Font(R.font.overpass_regular, FontWeight.Normal),
+    Font(R.font.overpass_medium, FontWeight.Medium),
+    Font(R.font.overpass_semibold, FontWeight.SemiBold),
+    Font(R.font.overpass_bold, FontWeight.Bold),
+)
+
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Overpass,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontSize = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = Overpass,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        letterSpacing = 0.25.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = Overpass,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        letterSpacing = 0.15.sp
+    ),
+    subtitle2 = TextStyle(
+        fontFamily = Overpass,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        letterSpacing = 0.1.sp
+    ),
+    h4 = TextStyle(
+        fontFamily = Overpass,
+        fontWeight = FontWeight.Light,
+        fontSize = 35.sp,
+        letterSpacing = 0.25.sp
+    ),
+    h5 = TextStyle(
+        fontFamily = Overpass,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp
+    ),
+    h6 = TextStyle(
+        fontFamily = Overpass,
+        fontWeight = FontWeight.Medium,
+        fontSize = 20.sp,
+        letterSpacing = 0.20.sp
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Overpass,
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+        fontSize = 12.sp,
+        letterSpacing = 0.4.sp
+    ),
 )
