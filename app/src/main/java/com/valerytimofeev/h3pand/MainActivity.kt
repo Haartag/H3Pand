2c0940e535e8ea311ca2ba79298d0ef436b046a1
llin first <90470991+Haartag@users.noreply.github.com>
2022-10-08 19:56:37 +0300

    Navigation

--- app/src/main/java/com/valerytimofeev/h3pand/MainActivity.kt
+++ app/src/main/java/com/valerytimofeev/h3pand/MainActivity.kt
@@ -7,7 +7,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.valerytimofeev.h3pand.ui.mapselection.MapSelectionScreen
 import com.valerytimofeev.h3pand.ui.pandcalculation.PandCalculationScreen
 import com.valerytimofeev.h3pand.ui.theme.H3PandTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,11 +26,36 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             H3PandTheme {
+                //Navigation
+                val navController = rememberNavController()
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    PandCalculationScreen()
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = "map_selection_screen"
+                    ) {
+                        composable("map_selection_screen") {
+                            MapSelectionScreen(navController = navController)
+                        }
+                        composable("pand_calculation_screen/{mapName}", arguments = listOf(
+                            navArgument("mapName") {
+                                type = NavType.StringType
+                            }
+                        )
+                        ) {
+                            val mapName = remember {
+                                it.arguments?.getString("mapName")
+                            }
+                            PandCalculationScreen(
+                                navController = navController,
+                                mapName = mapName ?: ""
+                            )
+                        }
+                    }
                 }
             }
         }
