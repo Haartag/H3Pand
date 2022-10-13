43c11e3767385d9ed1aad72d87639d7048354970
llin first <90470991+Haartag@users.noreply.github.com>
2022-10-14 00:49:27 +0300

    ConvertToSearchListUseCase and tests added.

---
+++ app/src/main/java/com/valerytimofeev/h3pand/domain/use_case/dialog_use_case/ConvertToSearchListUseCase.kt
@@ -0,0 +1,38 @@
+package com.valerytimofeev.h3pand.domain.use_case.dialog_use_case
+
+import com.valerytimofeev.h3pand.data.local.AdditionalValueItem
+import com.valerytimofeev.h3pand.data.local.Dwelling
+import com.valerytimofeev.h3pand.domain.model.SearchItem
+
+/**
+ * Merge [AdditionalValueItem] list and [Dwelling] list to [SearchItem] list.
+ */
+class ConvertToSearchListUseCase {
+    operator fun invoke(
+        addValueList: List<AdditionalValueItem>,
+        dwellingList: List<Dwelling>
+    ): List<SearchItem> {
+
+        val resultList = mutableListOf<SearchItem>()
+
+        resultList.addAll(addValueList.map {
+            SearchItem(
+                itemName = it.name,
+                isDwelling = false,
+                addItemValue = it.value
+            )
+        })
+        resultList.addAll(dwellingList.map {
+            SearchItem(
+                itemName = it.dwellingName,
+                isDwelling = true,
+                unitValue = it.AIValue,
+                weeklyGain = it.weeklyGain,
+                castle = it.castle
+            )
+        })
+
+        return resultList
+    }
+}
+
