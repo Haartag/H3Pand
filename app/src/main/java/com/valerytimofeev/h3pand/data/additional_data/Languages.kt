f2b400a54f75848a0404dbf8bd626a13c630d834
llin first <90470991+Haartag@users.noreply.github.com>
2022-10-18 17:56:09 +0300

    List of languages

---
+++ app/src/main/java/com/valerytimofeev/h3pand/data/additional_data/Languages.kt
@@ -0,0 +1,9 @@
+package com.valerytimofeev.h3pand.data.additional_data
+
+object Languages {
+
+    val languageList = listOf(
+        listOf("English", "Английский"),
+        listOf("Russian", "Русский"),
+    )
+}
