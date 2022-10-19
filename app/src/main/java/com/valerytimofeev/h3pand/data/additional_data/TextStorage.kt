783c4f694996b8f62494319769775085d2bdd6f0
llin first <90470991+Haartag@users.noreply.github.com>
2022-10-19 21:30:00 +0300

    All text variables with rus localization, TextWithLocalization DataClass.

---
+++ app/src/main/java/com/valerytimofeev/h3pand/data/additional_data/TextStorage.kt
@@ -0,0 +1,39 @@
+package com.valerytimofeev.h3pand.data.additional_data
+
+
+data class TextWithLocalization(
+    val enText: String,
+    val ruText: String
+)
+
+enum class TextStorage(
+    val text: TextWithLocalization
+) {
+    MapTitle(TextWithLocalization("Choose map", "Выбор карты")),
+
+    SettingsLanguageHint(
+        TextWithLocalization(
+            "Select language for application:",
+            "Выберите язык для приложения:"
+        )
+    ),
+    SettingsLanguageEng(TextWithLocalization("English", "Английский")),
+    SettingsLanguageRus(TextWithLocalization("Russian", "Русский")),
+
+    SheetTotalValue(TextWithLocalization("Total value:", "Общая ценность:")),
+    SheetWeekAndMonth(TextWithLocalization("month: %s, week: %s", "месяц: %s, неделя: %s")),
+    SheetNumberOfTownZones(
+        TextWithLocalization(
+            "Number of town's zones: %s",
+            "Количество зон города: %s"
+        )
+    ),
+    SheetZoneName(TextWithLocalization("Type of zone: %s", "Тип зоны: %s")),
+    SheetZoneTownName(TextWithLocalization("Town of zone: %s", "Город зоны: %s")),
+
+    DialogSearch(TextWithLocalization("Search", "Поиск")),
+
+    ItemGuard(TextWithLocalization("Guard: %s,", "Охрана: %s,")),
+    ItemMostLikely(TextWithLocalization("most likely %s", "вероятнее всего %s"))
+
+}
