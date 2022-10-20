b0ec243f57f5e6db8d5e10b080ffa7c68ab8ec21
llin first <90470991+Haartag@users.noreply.github.com>
2022-10-21 00:32:13 +0300

    Settings screen and ViewModel - language selection.

---
+++ app/src/main/java/com/valerytimofeev/h3pand/ui/settings/SettingsScreen.kt
@@ -0,0 +1,140 @@
+package com.valerytimofeev.h3pand.ui.settings
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.valerytimofeev.h3pand.ui.theme.SandboxButtonColor
+
+@Composable
+fun SettingsScreen(
+    navController: NavController,
+    settingsViewModel: SettingsViewModel = hiltViewModel()
+) {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(Modifier.fillMaxSize()) {
+            SettingsListItem { LanguageSettings() }
+        }
+        Column(modifier = Modifier.fillMaxSize()) {
+            Spacer(modifier = Modifier.fillMaxHeight(0.85f))
+            if (settingsViewModel.snackBarVisible.value) {
+                LanguageConfirmBox(navController)
+            }
+        }
+    }
+}
+
+@Composable
+fun LanguageSettings(
+    settingsViewModel: SettingsViewModel = hiltViewModel()
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = settingsViewModel.languageText)
+
+        Row(
+            modifier = Modifier.clickable {
+                settingsViewModel.languageDropDownState.value = true
+            }
+        ) {
+            Text(
+                text = settingsViewModel.currentLanguage.value
+            )
+            Box {
+                Icon(
+                    Icons.Default.KeyboardArrowDown,
+                    contentDescription = "Dropdown menu icon"
+                )
+                DropdownMenu(
+                    expanded = settingsViewModel.languageDropDownState.value,
+                    onDismissRequest = { settingsViewModel.languageDropDownState.value = false }
+                ) {
+
+                    settingsViewModel.languages.forEachIndexed { index, item ->
+                        DropdownMenuItem(
+                            onClick = {
+                                settingsViewModel.currentLanguage.value = item
+                                settingsViewModel.setLanguage(index)
+                                settingsViewModel.languageDropDownState.value = false
+                            }
+                        ) {
+                            Text(text = item)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SettingsListItem(
+    content: @Composable () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(96.dp),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(0.9f)
+                .height(1.dp)
+                .background(Color.Gray)
+        )
+        content()
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(0.9f)
+                .height(1.dp)
+                .background(Color.Gray)
+        )
+    }
+}
+
+@Composable
+fun LanguageConfirmBox(
+    navController: NavController,
+    settingsViewModel: SettingsViewModel = hiltViewModel()
+) {
+    Snackbar(
+        modifier = Modifier.padding(12.dp),
+        action = {
+            Text(
+                text = settingsViewModel.sandboxButtonText,
+                style = androidx.compose.ui.text.TextStyle(textDecoration = TextDecoration.Underline),
+                textAlign = TextAlign.Center,
+                color = SandboxButtonColor,
+                modifier = Modifier
+                    .fillMaxWidth(0.2f)
+                    .clickable {
+                        settingsViewModel.setLocale()
+                        navController.navigate("splash_screen")
+                    }
+            )
+        }
+    ) {
+        Text(
+            text = settingsViewModel.sandboxText,
+            modifier = Modifier.fillMaxWidth(0.8f)
+        )
+    }
+}
