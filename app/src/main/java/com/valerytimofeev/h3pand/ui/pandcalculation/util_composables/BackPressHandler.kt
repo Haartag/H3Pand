ddfc84997ef65c07979d37cb6f2f7e42924ab62a
llin first <90470991+Haartag@users.noreply.github.com>
2022-10-24 20:16:10 +0300

    BackStackHandler has been added.

---
+++ app/src/main/java/com/valerytimofeev/h3pand/ui/pandcalculation/util_composables/BackPressHandler.kt
@@ -0,0 +1,35 @@
+package com.valerytimofeev.h3pand.ui.pandcalculation.util_composables
+
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcher
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+
+/**
+ * Handle BackPress and replace it with [onBackPressed] lambda
+ */
+@Composable
+fun BackPressHandler(
+    backPressedDispatcher: OnBackPressedDispatcher? =
+        LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher,
+    onBackPressed: () -> Unit
+) {
+
+    val currentOnBackPressed = rememberUpdatedState(newValue = onBackPressed)
+    val backCallback = remember {
+        object: OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                currentOnBackPressed.value()
+            }
+        }
+    }
+    DisposableEffect(key1 = backPressedDispatcher) {
+        backPressedDispatcher?.addCallback(backCallback)
+        onDispose {
+            backCallback.remove()
+        }
+    }
+}
