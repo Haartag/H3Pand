ee303320d4cc52f59d17a9f5f504b12414bf795d
llin first <90470991+Haartag@users.noreply.github.com>
2022-10-25 14:02:55 +0300

    Font changed to Ubuntu

--- app/src/main/java/com/valerytimofeev/h3pand/ui/theme/Type.kt
+++ app/src/main/java/com/valerytimofeev/h3pand/ui/theme/Type.kt
@@ -8,58 +8,57 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.valerytimofeev.h3pand.R
 
-val Overpass = FontFamily(
-    Font(R.font.overpass_light, FontWeight.Light),
-    Font(R.font.overpass_regular, FontWeight.Normal),
-    Font(R.font.overpass_medium, FontWeight.Medium),
-    Font(R.font.overpass_semibold, FontWeight.SemiBold),
-    Font(R.font.overpass_bold, FontWeight.Bold),
+val Ubuntu = FontFamily(
+    Font(R.font.ubuntu_light, FontWeight.Light),
+    Font(R.font.ubuntu_condensed, FontWeight.Normal),
+    Font(R.font.ubuntu_medium, FontWeight.Medium),
+    Font(R.font.ubuntu_bold, FontWeight.Bold),
 )
 
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = Overpass,
+        fontFamily = Ubuntu,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         letterSpacing = 0.5.sp
     ),
     body2 = TextStyle(
-        fontFamily = Overpass,
+        fontFamily = Ubuntu,
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp,
         letterSpacing = 0.25.sp
     ),
     subtitle1 = TextStyle(
-        fontFamily = Overpass,
-        fontWeight = FontWeight.Normal,
+        fontFamily = Ubuntu,
+        fontWeight = FontWeight.Medium,
         fontSize = 16.sp,
         letterSpacing = 0.15.sp
     ),
     subtitle2 = TextStyle(
-        fontFamily = Overpass,
-        fontWeight = FontWeight.Normal,
+        fontFamily = Ubuntu,
+        fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         letterSpacing = 0.1.sp
     ),
     h4 = TextStyle(
-        fontFamily = Overpass,
+        fontFamily = Ubuntu,
         fontWeight = FontWeight.Light,
         fontSize = 35.sp,
         letterSpacing = 0.25.sp
     ),
     h5 = TextStyle(
-        fontFamily = Overpass,
+        fontFamily = Ubuntu,
         fontWeight = FontWeight.Normal,
         fontSize = 24.sp
     ),
     h6 = TextStyle(
-        fontFamily = Overpass,
+        fontFamily = Ubuntu,
         fontWeight = FontWeight.Medium,
         fontSize = 20.sp,
         letterSpacing = 0.20.sp
     ),
     caption = TextStyle(
-        fontFamily = Overpass,
+        fontFamily = Ubuntu,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp,
         letterSpacing = 0.4.sp
