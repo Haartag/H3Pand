184819997c4bc173291f23f510ba4c9cff369465
llin first <90470991+Haartag@users.noreply.github.com>
2022-10-29 20:52:04 +0300

    TopAppBar update

--- app/src/main/java/com/valerytimofeev/h3pand/ui/topbar/TopAppBar.kt
+++ app/src/main/java/com/valerytimofeev/h3pand/ui/topbar/TopAppBar.kt
@@ -1,24 +1,39 @@
 package com.valerytimofeev.h3pand.ui.topbar
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
+import com.valerytimofeev.h3pand.R
 import com.valerytimofeev.h3pand.ui.theme.Typography
 
 @Composable
 fun MainTopBar(
     title: String = "",
     titleStyle: TextStyle = Typography.h5,
-    buttonIcon: @Composable () -> Unit = {},
+    buttonIcon: @Composable () -> Unit = {
+        Icon(
+            painterResource(id = R.drawable.ic_icon),
+            contentDescription = "App icon",
+            tint = Color.DarkGray
+        )
+    },
     onButtonClicked: (() -> Unit)? = null,
-    backgroundColor: Color = Color.LightGray,
-    additionalInfo: @Composable () -> Unit = {}
+    backgroundColor: Color = Color.White,
+    additionalInfo: @Composable () -> Unit = {
+        Icon(
+            painterResource(id = R.drawable.ic_icon),
+            contentDescription = "App icon",
+            tint = Color.DarkGray
+        )
+    }
 ) {
     TopAppBar(
         title = {
