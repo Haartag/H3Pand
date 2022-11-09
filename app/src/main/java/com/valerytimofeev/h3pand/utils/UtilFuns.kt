47d800a82f624b3efeb09dbdc5fe4a41b3e043b0
llin first <90470991+Haartag@users.noreply.github.com>
2022-11-09 19:28:52 +0300

    Round to 2 digits extension

--- app/src/main/java/com/valerytimofeev/h3pand/utils/UtilFuns.kt
+++ app/src/main/java/com/valerytimofeev/h3pand/utils/UtilFuns.kt
@@ -1,9 +1,14 @@
 package com.valerytimofeev.h3pand.utils
 
 import kotlin.math.ceil
+import kotlin.math.roundToInt
 
 //Math
 
 fun Double.ceilToInt(): Int {
     return ceil(this).toInt()
+}
+
+fun Double.roundToTwoDigits(): Double {
+    return (this * 100).roundToInt() / 100.0
 }
