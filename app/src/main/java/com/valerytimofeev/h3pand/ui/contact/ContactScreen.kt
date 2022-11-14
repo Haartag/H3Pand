4d29d51d291f2c8dc35479b1eac2f97e13e0204a
llin first <90470991+Haartag@users.noreply.github.com>
2022-11-15 00:56:08 +0300

    Added discord group button.

--- app/src/main/java/com/valerytimofeev/h3pand/ui/contact/ContactScreen.kt
+++ app/src/main/java/com/valerytimofeev/h3pand/ui/contact/ContactScreen.kt
@@ -8,11 +8,15 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.MailOutline
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.valerytimofeev.h3pand.R
 import com.valerytimofeev.h3pand.ui.topbar.MainTopBar
 
 @Composable
@@ -36,20 +40,46 @@ fun ContactScreen(
             onButtonClicked = { navController.popBackStack() }
         )
         Spacer(modifier = Modifier.height(24.dp))
-
-        Row(
-            modifier = Modifier
-                .height(56.dp)
-                .clickable {
-                    contactViewModel.sendEmail(context = context)
-                }
-        ) {
-            Icon(
-                Icons.Default.MailOutline,
-                contentDescription = "Mail"
-            )
-            Text(text = contactViewModel.getLocalizedTextUseCase(contactViewModel.mailText))
+        
+        Column() {
+            Row(
+                modifier = Modifier
+                    .height(56.dp)
+                    .clickable {
+                        contactViewModel.sendEmail(context = context)
+                    },
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Spacer(modifier = Modifier.width(24.dp))
+                Icon(
+                    Icons.Default.MailOutline,
+                    modifier = Modifier
+                        .size(24.dp),
+                    contentDescription = "Mail"
+                )
+                Spacer(modifier = Modifier.width(24.dp))
+                Text(text = contactViewModel.getLocalizedTextUseCase(contactViewModel.mailText))
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                modifier = Modifier
+                    .height(56.dp)
+                    .clickable {
+                        contactViewModel.openDiscord(context = context)
+                    },
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Spacer(modifier = Modifier.width(24.dp))
+                Icon(
+                    painterResource(id = R.drawable.ic_discord),
+                    modifier = Modifier
+                        .size(24.dp),
+                    contentDescription = "Discord",
+                    tint = Color(0xFF5865F2)
+                )
+                Spacer(modifier = Modifier.width(24.dp))
+                Text(text = contactViewModel.getLocalizedTextUseCase(contactViewModel.discordText))
+            }
         }
-
     }
 }
--- app/src/main/java/com/valerytimofeev/h3pand/ui/contact/ContactViewModel.kt
+++ app/src/main/java/com/valerytimofeev/h3pand/ui/contact/ContactViewModel.kt
@@ -1,6 +1,9 @@
 package com.valerytimofeev.h3pand.ui.contact
 
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import com.valerytimofeev.h3pand.data.additional_data.TextStorage
@@ -22,6 +25,10 @@ class ContactViewModel @Inject constructor(
         enText = "Contact by email",
         ruText = "Связаться по электронной почте"
     )
+    val discordText = TextWithLocalization(
+        enText = "Discord group",
+        ruText = "Группа в Discord"
+    )
 
     fun sendEmail(context: Context) {
         val result = sendEmailUseCase(context)
@@ -31,6 +38,15 @@ class ContactViewModel @Inject constructor(
                 "Unexpected error when sending an email",
                 Toast.LENGTH_SHORT)
                 .show()
+        }
+    }
+
+    fun openDiscord(context: Context) {
+        val discordIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://discord.gg/jFs8vU7tpU"))
+        try {
+            context.startActivity(discordIntent)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(context, "Something went wrong...", Toast.LENGTH_SHORT).show()
         }
     }
 }
