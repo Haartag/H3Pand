34dff5ebe9e514814f0ce622288af290ccb2c961
llin first <90470991+Haartag@users.noreply.github.com>
2022-11-15 19:27:56 +0300

    ItemWithFrequency rename

---
+++ app/src/main/java/com/valerytimofeev/h3pand/data/additional_data/ItemWithFrequency.kt
@@ -0,0 +1,40 @@
+package com.valerytimofeev.h3pand.data.additional_data
+
+/**
+ * Pair of value and frequency of items to determine the chance of dropping boxes.
+ * Contains items with variable value not represented in the database and some other things.
+ */
+data class ItemWithFrequency(
+    val value: Int,
+    val frequency: Int
+) {
+    companion object {
+        /**
+         * List of special value/frequency
+         */
+        val specialItemsWithFrequency = listOf(
+            //Random artifacts
+            ItemWithFrequency(2000, 150),
+            ItemWithFrequency(5000, 150),
+            ItemWithFrequency(10000, 150),
+            ItemWithFrequency(20000, 150),
+            //Prisons
+            ItemWithFrequency(2500, 30),
+            ItemWithFrequency(5000, 30),
+            ItemWithFrequency(10000, 30),
+            ItemWithFrequency(20000, 30),
+            ItemWithFrequency(30000, 30),
+            //Seer`s hut
+            //Gold
+            ItemWithFrequency(2000, 10),
+            ItemWithFrequency(5333, 10),
+            ItemWithFrequency(8666, 10),
+            ItemWithFrequency(12000, 10),
+            //Exp.
+            ItemWithFrequency(2000, 10),
+            ItemWithFrequency(5333, 10),
+            ItemWithFrequency(8666, 10),
+            ItemWithFrequency(12000, 10),
+        )
+    }
+}
