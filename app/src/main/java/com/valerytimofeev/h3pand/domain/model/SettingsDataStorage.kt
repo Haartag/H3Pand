be2b8e321a4d4b4613a9066362b61d0cbd265c11
llin first <90470991+Haartag@users.noreply.github.com>
2022-11-18 19:26:37 +0300

    New settings preference - MIN_PERCENT

--- app/src/main/java/com/valerytimofeev/h3pand/domain/model/SettingsDataStorage.kt
+++ app/src/main/java/com/valerytimofeev/h3pand/domain/model/SettingsDataStorage.kt
@@ -2,20 +2,21 @@ package com.valerytimofeev.h3pand.domain.model
 
 import android.content.Context
 import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.booleanPreferencesKey
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.*
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
+/**
+ * Preferences data store for settings
+ */
 class SettingsDataStorage(private val context: Context ) {
 
     companion object {
         val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
         val LANGUAGE = intPreferencesKey("language")
         val LIST_TYPE = booleanPreferencesKey("listType")
+        val MIN_PERCENT = floatPreferencesKey("minPercent")
     }
 
     val getLanguage: Flow<Int?> = context.dataStore.data.map { preferences ->
@@ -24,6 +25,10 @@ class SettingsDataStorage(private val context: Context ) {
 
     val getListType: Flow<Boolean> = context.dataStore.data.map { preferences ->
         preferences[LIST_TYPE] ?: true
+    }
+
+    val getMinPercent: Flow<Float> = context.dataStore.data.map { preferences ->
+        preferences[MIN_PERCENT] ?: 0.05f
     }
 
     suspend fun setLanguage(language: Int) {
@@ -35,6 +40,12 @@ class SettingsDataStorage(private val context: Context ) {
     suspend fun setListType(type: Boolean) {
         context.dataStore.edit { preferences ->
             preferences[LIST_TYPE] = type
+        }
+    }
+
+    suspend fun setMinPercent(minPercent: Float) {
+        context.dataStore.edit { preferences ->
+            preferences[MIN_PERCENT] = minPercent
         }
     }
 }
