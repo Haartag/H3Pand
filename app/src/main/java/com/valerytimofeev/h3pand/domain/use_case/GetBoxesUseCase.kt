e1507dfd4dbe60a8d2bf482675f74ffc3643ad90
llin first <90470991+Haartag@users.noreply.github.com>
2022-11-19 18:12:12 +0300

    GetBoxesUseCase rework - getAllAvailableBoxesUseCase and getValidBoxesAndGuardsUseCase instead of getBoxForGuardRangeUseCase, exact guard support. Updated tests.

--- app/src/main/java/com/valerytimofeev/h3pand/domain/use_case/GetBoxesUseCase.kt
+++ app/src/main/java/com/valerytimofeev/h3pand/domain/use_case/GetBoxesUseCase.kt
@@ -1,12 +1,11 @@
 package com.valerytimofeev.h3pand.domain.use_case
 
-import com.valerytimofeev.h3pand.data.local.Guard
-import com.valerytimofeev.h3pand.domain.model.BoxWithDropPercent
+import com.valerytimofeev.h3pand.data.local.additional_data.Difficult.Companion.getDifficultForZone
+import com.valerytimofeev.h3pand.data.local.additional_data.MapSettings.Companion.getMapSettings
+import com.valerytimofeev.h3pand.data.local.database.Guard
+import com.valerytimofeev.h3pand.domain.model.BoxWithDropChance
 import com.valerytimofeev.h3pand.utils.Constants.MAX_GENERATED_GUARD
-import com.valerytimofeev.h3pand.utils.Constants.MIN_PAND_VALUE
-import com.valerytimofeev.h3pand.domain.model.Difficult.Companion.getDifficultForZone
-import com.valerytimofeev.h3pand.data.additional_data.GuardRanges
-import com.valerytimofeev.h3pand.data.additional_data.MapSettings.Companion.getMapSettings
+import com.valerytimofeev.h3pand.utils.Constants.MIN_PERCENT
 import com.valerytimofeev.h3pand.utils.Resource
 import javax.inject.Inject
 import kotlin.math.roundToInt
@@ -16,20 +15,21 @@ import kotlin.math.roundToInt
  */
 class GetBoxesUseCase @Inject constructor(
     private val getGuardCharacteristicsUseCase: GetGuardCharacteristicsUseCase,
-    private val getBoxForGuardRangeUseCase: GetBoxForGuardRangeUseCase,
     private val getBoxWithPercentUseCase: GetBoxWithPercentUseCase,
     private val getZoneValueRangeUseCase: GetZoneValueRangeUseCase,
+    private val getAllAvailableBoxesUseCase: GetAllAvailableBoxesUseCase,
+    private val getValidBoxesAndGuardsUseCase: GetValidBoxesAndGuardsUseCase
 ) {
     suspend operator fun invoke(
         guardUnit: Guard,
         castle: Int,
         castleZones: Int,
-        guardRangeIndex: Int,
+        guardRange: IntRange,
         zoneType: Int,
         additionalValue: Int,
         week: Int,
         mapName: String
-    ): Resource<List<BoxWithDropPercent>> {
+    ): Resource<List<BoxWithDropChance>> {
 
         val mapSettings = getMapSettings(mapName)
 
@@ -39,7 +39,7 @@ class GetBoxesUseCase @Inject constructor(
         val valueRangesForZone = mapSettings.valueRanges.getOrNull(zoneType)?.valueRange
             ?: return Resource.error("An unknown error occurred", null)
 
-        var valueRange = getZoneValueRangeUseCase(valueRangesForZone).data
+        val valueRange = getZoneValueRangeUseCase(valueRangesForZone).data
             ?: return Resource.error("An unknown error occurred", null)
 
         val zones = mapSettings.numberOfZones
@@ -47,69 +47,75 @@ class GetBoxesUseCase @Inject constructor(
         val difficult = getDifficultForZone(zoneLvl)
             ?: return Resource.error("An unknown error occurred", null)
 
-        val chosenGuardRange = GuardRanges.range[guardRangeIndex] //TODO make guard range IntRange, not index of IntRange
-            ?: return Resource.error("An unknown error occurred", null)
+        val weekCorrectedMinGuardValue = guardRange.first.weekCorrection(week)
+        val weekCorrectedMaxGuardValue = guardRange.last.weekCorrection(week)
 
-        val weekCorrectedMinGuardValue = chosenGuardRange.first.weekCorrection(week)
-        val weekCorrectedMaxGuardValue = chosenGuardRange.last.weekCorrection(week)
-
-        if (valueRange.first < MIN_PAND_VALUE) valueRange = MIN_PAND_VALUE..valueRange.last
-
-        val guardRange = getGuardCharacteristicsUseCase( // Guard range for input data
+        val guardCharacteristics = getGuardCharacteristicsUseCase(
+            // Guard range for input data
             weekCorrectedMin = weekCorrectedMinGuardValue,
             weekCorrectedMax = weekCorrectedMaxGuardValue,
             minGuardOnMap = ((guardUnit.minOnMap + guardUnit.maxOnMap.toDouble()) / 2.0).toInt(),
             maxGuardOnMap = MAX_GENERATED_GUARD,
         )
-        if (guardRange.data == null) return Resource.error(
-            guardRange.message
+        if (guardCharacteristics.data == null) return Resource.error(
+            guardCharacteristics.message
                 ?: "An unknown error occurred: error code 01GB", null
         )
 
-        val boxes = getBoxForGuardRangeUseCase( //All potentially valid boxes for guardRange
-            difficult = difficult,
-            guardRange = guardRange.data,
-            guardValue = guardUnit.AIValue,
-            additionalValue = additionalValue,
+        /**
+         * List of all boxes, including units of the selected zone
+         */
+        val boxes = getAllAvailableBoxesUseCase(
             castleZones = castleZones,
             zones = zones,
             castle = castle
         )
-
         if (boxes.data == null) return Resource.error(
             boxes.message
                 ?: "An unknown error occurred: error code 02GB", null
         )
+        /**
+         * All boxes with a calculated number of guards (without week correction, first week value)
+         */
+        val boxesWithGuards = getValidBoxesAndGuardsUseCase(
+            boxes.data,
+            guardUnit,
+            additionalValue,
+            difficult,
+            valueRange
+        )
 
+        val boxesWithPercent = mutableListOf<BoxWithDropChance>()
 
-        val boxesWithPercent = mutableListOf<BoxWithDropPercent>()
-
-        for (index in boxes.data.indices) { //Take boxes that fit the restrictions of the zone and calculate drop chances.
-            if (boxes.data[index].value + additionalValue in valueRange) {
+        for (index in boxesWithGuards.indices) { //Take boxes that fit the restrictions of the zone and calculate drop chances.
+            if (boxesWithGuards[index].box.value + additionalValue in valueRange) {
                 boxesWithPercent.add(
                     getBoxWithPercentUseCase(
-                        boxValueItem = boxes.data[index],
-                        guardValue = guardRange.data,
-                        difficult = difficult,
-                        unitValue = guardUnit.AIValue,
+                        boxesWithGuards[index],
+                        guardValue = guardCharacteristics.data,
                         week = week,
-                        additionalValue = additionalValue,
-                        chosenGuardRange = chosenGuardRange,
+                        chosenGuardRange = guardRange,
                         castle = castle,
                         mapSettings = mapSettings,
                         numberOfZones = zones.toFloat(),
                         numberOfUnitZones = castleZones.toFloat(),
-                        zoneType = zoneType
+                        zoneType = zoneType,
+                        exactlyGuard = guardRange.first == guardRange.last
                     ).data ?: continue
                 )
             } else continue
         }
 
         val percentSum = boxesWithPercent.sumOf { it.dropChance }
+        if (percentSum == 0.0) return Resource.error(
+            "It is impossible to find boxes for this kind or amount of guard",
+            null
+        )
 
         boxesWithPercent.forEach {
             it.dropChance = ((it.dropChance * 100 / percentSum) * 10).roundToInt() / 10.0
         }
+        boxesWithPercent.removeAll { it.dropChance < MIN_PERCENT }
 
         boxesWithPercent.sortByDescending { it.dropChance }
 
--- app/src/test/java/com/valerytimofeev/h3pand/domain/use_case/GetBoxesUseCaseTest.kt
+++ app/src/test/java/com/valerytimofeev/h3pand/domain/use_case/GetBoxesUseCaseTest.kt
@@ -1,10 +1,10 @@
 package com.valerytimofeev.h3pand.domain.use_case
 
 import com.google.common.truth.Truth.assertThat
-import com.valerytimofeev.h3pand.data.additional_data.TextWithLocalization
-import com.valerytimofeev.h3pand.data.local.Guard
+import com.valerytimofeev.h3pand.data.local.additional_data.TextWithLocalization
+import com.valerytimofeev.h3pand.data.local.database.Guard
+import com.valerytimofeev.h3pand.domain.model.BoxWithDropChance
 import com.valerytimofeev.h3pand.repositories.local.FakePandRepository
-import com.valerytimofeev.h3pand.domain.model.BoxWithDropPercent
 import com.valerytimofeev.h3pand.utils.Resource
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -16,30 +16,34 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Test
 
+
 @ExperimentalCoroutinesApi
 class GetBoxesUseCaseTest {
 
     private val mainThreadSurrogate = newSingleThreadContext("UI thread")
 
     private lateinit var getBox: GetBoxesUseCase
-    private lateinit var getBoxForGuardRange: GetBoxForGuardRangeUseCase
     private lateinit var getGuardCharacteristicsUseCase: GetGuardCharacteristicsUseCase
     private lateinit var getBoxWithPercentUseCase: GetBoxWithPercentUseCase
     private lateinit var getUnitDropCoefficientUseCase: GetUnitDropCoefficientUseCase
     private lateinit var getZoneValueRangeUseCase: GetZoneValueRangeUseCase
+    private lateinit var getAllAvailableBoxesUseCase: GetAllAvailableBoxesUseCase
+    private lateinit var getValidBoxesAndGuardsUseCase: GetValidBoxesAndGuardsUseCase
 
     @Before
     fun setup() {
-        getBoxForGuardRange = GetBoxForGuardRangeUseCase(FakePandRepository())
         getGuardCharacteristicsUseCase = GetGuardCharacteristicsUseCase()
         getUnitDropCoefficientUseCase = GetUnitDropCoefficientUseCase(FakePandRepository())
         getZoneValueRangeUseCase = GetZoneValueRangeUseCase()
+        getAllAvailableBoxesUseCase = GetAllAvailableBoxesUseCase(FakePandRepository())
+        getValidBoxesAndGuardsUseCase = GetValidBoxesAndGuardsUseCase()
         getBoxWithPercentUseCase = GetBoxWithPercentUseCase(getUnitDropCoefficientUseCase)
         getBox = GetBoxesUseCase(
             getGuardCharacteristicsUseCase,
-            getBoxForGuardRange,
             getBoxWithPercentUseCase,
-            getZoneValueRangeUseCase
+            getZoneValueRangeUseCase,
+            getAllAvailableBoxesUseCase,
+            getValidBoxesAndGuardsUseCase
         )
 
         Dispatchers.setMain(mainThreadSurrogate)
@@ -59,7 +63,7 @@ class GetBoxesUseCaseTest {
             guardUnit = guardUnit,
             castle = 1,
             castleZones = 1,
-            guardRangeIndex = 4,
+            guardRange = 20..49,
             zoneType = 0,
             additionalValue = 0,
             week = 1,
@@ -68,24 +72,24 @@ class GetBoxesUseCaseTest {
 
         assertThat(result).isEqualTo(
             Resource.success(
-                listOf<BoxWithDropPercent>(
-                    BoxWithDropPercent(
+                listOf(
+                    BoxWithDropChance(
                         name = TextWithLocalization("item 2", "предмет 2"),
-                        dropChance = 85.2,
+                        dropChance = 85.0,
                         mostLikelyGuard = 34,
                         range = 26..42,
                         type = "Exp",
                         img = "img"
                     ),
-                    BoxWithDropPercent(
+                    BoxWithDropChance(
                         name = TextWithLocalization("test name 1 60", "тестовое имя 1 60"),
-                        dropChance = 14.5,
+                        dropChance = 14.8,
                         mostLikelyGuard = 25,
                         range = 20..31,
                         type = "Unit",
                         img = "img1"
                     ),
-                    BoxWithDropPercent(
+                    BoxWithDropChance(
                         name = TextWithLocalization("item 3", "предмет 3"),
                         dropChance = 0.2,
                         mostLikelyGuard = 49,
@@ -106,7 +110,7 @@ class GetBoxesUseCaseTest {
             guardUnit = guardUnit,
             castle = 5,
             castleZones = 1,
-            guardRangeIndex = 1,
+            guardRange = 1..4,
             zoneType = 0,
             additionalValue = 0,
             week = 1,
@@ -115,10 +119,18 @@ class GetBoxesUseCaseTest {
 
         assertThat(result).isEqualTo(
             Resource.success(
-                listOf<BoxWithDropPercent>(
-                    BoxWithDropPercent(
+                listOf(
+                    BoxWithDropChance(
+                        name = TextWithLocalization("test name 8 15", "тестовое имя 8 15"),
+                        dropChance = 52.2,
+                        mostLikelyGuard = 4,
+                        range = 4..4,
+                        type = "Unit",
+                        img = "img3"
+                    ),
+                    BoxWithDropChance(
                         name = TextWithLocalization("item 7", "предмет 7"),
-                        dropChance = 100.0,
+                        dropChance = 47.8,
                         mostLikelyGuard = 4,
                         range = 4..4,
                         type = "Gold",
@@ -137,7 +149,7 @@ class GetBoxesUseCaseTest {
             guardUnit = guardUnit,
             castle = 1,
             castleZones = 1,
-            guardRangeIndex = 4,
+            guardRange = 20..49,
             zoneType = 0,
             additionalValue = 0,
             week = 3,
@@ -146,20 +158,20 @@ class GetBoxesUseCaseTest {
 
         assertThat(result).isEqualTo(
             Resource.success(
-                listOf<BoxWithDropPercent>(
-                    BoxWithDropPercent(
+                listOf(
+                    BoxWithDropChance(
                         name = TextWithLocalization("item 2", "предмет 2"),
-                        dropChance = 84.9,
-                        mostLikelyGuard = 42,
-                        range = 32..49,
+                        dropChance = 85.0,
+                        mostLikelyGuard = 41,
+                        range = 31..49,
                         type = "Exp",
                         img = "img"
                     ),
-                    BoxWithDropPercent(
+                    BoxWithDropChance(
                         name = TextWithLocalization("test name 1 60", "тестовое имя 1 60"),
-                        dropChance = 15.1,
-                        mostLikelyGuard = 31,
-                        range = 25..38,
+                        dropChance = 15.0,
+                        mostLikelyGuard = 30,
+                        range = 24..37,
                         type = "Unit",
                         img = "img1"
                     )
@@ -176,7 +188,7 @@ class GetBoxesUseCaseTest {
             guardUnit = guardUnit,
             castle = 1,
             castleZones = 1,
-            guardRangeIndex = 4,
+            guardRange = 20..49,
             zoneType = 0,
             additionalValue = 0,
             week = 15,
@@ -194,7 +206,7 @@ class GetBoxesUseCaseTest {
             guardUnit = guardUnit,
             castle = 1,
             castleZones = 1,
-            guardRangeIndex = 4,
+            guardRange = 20..49,
             zoneType = 0,
             additionalValue = 5000,
             week = 1,
@@ -203,8 +215,8 @@ class GetBoxesUseCaseTest {
 
         assertThat(result).isEqualTo(
             Resource.success(
-                listOf<BoxWithDropPercent>(
-                    BoxWithDropPercent(
+                listOf(
+                    BoxWithDropChance(
                         name = TextWithLocalization("item 1", "предмет 1"),
                         dropChance = 100.0,
                         mostLikelyGuard = 49,
@@ -225,13 +237,13 @@ class GetBoxesUseCaseTest {
             guardUnit = guardUnit,
             castle = 1,
             castleZones = 1,
-            guardRangeIndex = 4,
+            guardRange = 20..49,
             zoneType = 0,
             additionalValue = 22000,
             week = 1,
             mapName = "JC"
         )
-        assertThat(result).isEqualTo(Resource.error(msg = "No boxes found", data = null))
+        assertThat(result).isEqualTo(Resource.error(msg = "It is impossible to find boxes for this kind or amount of guard", data = null))
     }
 
     @Test
@@ -242,7 +254,7 @@ class GetBoxesUseCaseTest {
             guardUnit = guardUnit,
             castle = 1,
             castleZones = 1,
-            guardRangeIndex = 4,
+            guardRange = 20..49,
             zoneType = 0,
             additionalValue = 4000,
             week = 3,
@@ -250,12 +262,12 @@ class GetBoxesUseCaseTest {
         )
         assertThat(result).isEqualTo(
             Resource.success(
-                listOf<BoxWithDropPercent>(
-                    BoxWithDropPercent(
+                listOf(
+                    BoxWithDropChance(
                         name = TextWithLocalization("item 1", "предмет 1"),
                         dropChance = 100.0,
                         mostLikelyGuard = 49,
-                        range = 46..49,
+                        range = 45..49,
                         type = "Gold",
                         img = "img"
                     ),
@@ -272,7 +284,7 @@ class GetBoxesUseCaseTest {
             guardUnit = guardUnit,
             castle = 1,
             castleZones = 1,
-            guardRangeIndex = 4,
+            guardRange = 20..49,
             zoneType = 0,
             additionalValue = 20000,
             week = 15,
@@ -287,9 +299,9 @@ class GetBoxesUseCaseTest {
         val guardUnit = Guard("testUnit", "тестЮнит", 190, 20, 30, "img")
         val result = getBox(
             guardUnit = guardUnit,
-            castle = 3,
+            castle = 10,
             castleZones = 1,
-            guardRangeIndex = 4,
+            guardRange = 20..49,
             zoneType = 0,
             additionalValue = 0,
             week = 1,
@@ -298,24 +310,24 @@ class GetBoxesUseCaseTest {
 
         assertThat(result).isEqualTo(
             Resource.success(
-                listOf<BoxWithDropPercent>(
-                    BoxWithDropPercent(
+                listOf(
+                    BoxWithDropChance(
                         name = TextWithLocalization("item 2", "предмет 2"),
-                        dropChance = 90.9,
+                        dropChance = 90.8,
                         mostLikelyGuard = 34,
                         range = 26..42,
                         type = "Exp",
                         img = "img"
                     ),
-                    BoxWithDropPercent(
+                    BoxWithDropChance(
                         name = TextWithLocalization("test name 4 45", "тестовое имя 4 45"),
-                        dropChance = 8.8,
+                        dropChance = 8.9,
                         mostLikelyGuard = 46,
                         range = 35..49,
                         type = "Unit",
                         img = "img1"
                     ),
-                    BoxWithDropPercent(
+                    BoxWithDropChance(
                         name = TextWithLocalization("item 3", "предмет 3"),
                         dropChance = 0.3,
                         mostLikelyGuard = 49,
@@ -336,7 +348,7 @@ class GetBoxesUseCaseTest {
             guardUnit = guardUnit,
             castle = 1,
             castleZones = 3,
-            guardRangeIndex = 4,
+            guardRange = 20..49,
             zoneType = 0,
             additionalValue = 0,
             week = 1,
@@ -345,24 +357,24 @@ class GetBoxesUseCaseTest {
 
         assertThat(result).isEqualTo(
             Resource.success(
-                listOf<BoxWithDropPercent>(
-                    BoxWithDropPercent(
+                listOf(
+                    BoxWithDropChance(
                         name = TextWithLocalization("item 2", "предмет 2"),
-                        dropChance = 87.4,
+                        dropChance = 87.0,
                         mostLikelyGuard = 34,
                         range = 26..42,
                         type = "Exp",
                         img = "img"
                     ),
-                    BoxWithDropPercent(
+                    BoxWithDropChance(
                         name = TextWithLocalization("test name 1 60", "тестовое имя 1 60"),
-                        dropChance = 12.4,
+                        dropChance = 12.8,
                         mostLikelyGuard = 36,
                         range = 27..45,
                         type = "Unit",
                         img = "img1"
                     ),
-                    BoxWithDropPercent(
+                    BoxWithDropChance(
                         name = TextWithLocalization("item 3", "предмет 3"),
                         dropChance = 0.2,
                         mostLikelyGuard = 49,
@@ -383,26 +395,26 @@ class GetBoxesUseCaseTest {
             guardUnit = guardUnit,
             castle = 1,
             castleZones = 10,
-            guardRangeIndex = 4,
+            guardRange = 20..49,
             zoneType = 0,
             additionalValue = 0,
             week = 1,
             mapName = "JC"
         )
 
-        assertThat(result).isEqualTo(Resource.error(msg = "Error: Wrong map settings", data = null))
+        assertThat(result).isEqualTo(Resource.error(msg = "An unknown error occurred", data = null))
     }
 
     @Test
-    fun `Get Boxes, valid input, different zone type, returns success`() = runTest {
+    fun `Get Boxes, exact guard, returns success`() = runTest {
 
-        val guardUnit = Guard("testUnit", "тестЮнит", 6000, 3, 8, "img")
+        val guardUnit = Guard("testUnit", "тестЮнит", 190, 20, 30, "img")
         val result = getBox(
             guardUnit = guardUnit,
-            castle = 5,
+            castle = 1,
             castleZones = 1,
-            guardRangeIndex = 1,
-            zoneType = 1,
+            guardRange = 28..28,
+            zoneType = 0,
             additionalValue = 0,
             week = 1,
             mapName = "JC"
@@ -410,21 +422,64 @@ class GetBoxesUseCaseTest {
 
         assertThat(result).isEqualTo(
             Resource.success(
-                listOf<BoxWithDropPercent>(
-                    BoxWithDropPercent(
-                        name = TextWithLocalization("item 4", "предмет 4"),
-                        dropChance = 100.0,
-                        mostLikelyGuard = 4,
-                        range = 4..4,
-                        type = "Gold",
+                listOf(
+                    BoxWithDropChance(
+                        name = TextWithLocalization("item 2", "предмет 2"),
+                        dropChance = 75.2,
+                        mostLikelyGuard = 28,
+                        range = 26..42,
+                        type = "Exp",
                         img = "img"
+                    ),
+                    BoxWithDropChance(
+                        name = TextWithLocalization("test name 1 60", "тестовое имя 1 60"),
+                        dropChance = 24.8,
+                        mostLikelyGuard = 28,
+                        range = 19..31,
+                        type = "Unit",
+                        img = "img1"
                     )
                 )
             )
         )
     }
 
+    @Test
+    fun `Get Boxes, exact guard, week correction, returns success`() = runTest {
 
+        val guardUnit = Guard("testUnit", "тестЮнит", 190, 20, 30, "img")
+        val result = getBox(
+            guardUnit = guardUnit,
+            castle = 1,
+            castleZones = 1,
+            guardRange = 28..28,
+            zoneType = 0,
+            additionalValue = 0,
+            week = 2,
+            mapName = "JC"
+        )
 
-
+        assertThat(result).isEqualTo(
+            Resource.success(
+                listOf(
+                    BoxWithDropChance(
+                        name = TextWithLocalization("test name 1 60", "тестовое имя 1 60"),
+                        dropChance = 59.8,
+                        mostLikelyGuard = 28,
+                        range = 20..34,
+                        type = "Unit",
+                        img = "img1"
+                    ),
+                    BoxWithDropChance(
+                        name = TextWithLocalization("item 2", "предмет 2"),
+                        dropChance = 40.2,
+                        mostLikelyGuard = 28,
+                        range = 28..46,
+                        type = "Exp",
+                        img = "img"
+                    )
+                )
+            )
+        )
+    }
 }
